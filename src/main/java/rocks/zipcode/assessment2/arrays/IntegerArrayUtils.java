0313ea9bf72b01d0a4f9efca7cc7ee4dfe27bbfa
Nirmala <nirmala.talari@gmail.com>
2019-03-04 10:59:22 -0500

    push3

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -8,32 +8,31 @@ import java.util.Arrays;
  */
 public class IntegerArrayUtils {
     /**
-     * @param integerArray - array to have value added to it
+     * @param integerArray   - array to have value added to it
      * @param valueToBeAdded - value to be added to the end of the array
      * @return - identical array with one additional element of `valueToBeAdded` at the end of the array
      */
     public static Integer[] add(Integer[] integerArray, Integer valueToBeAdded) {
         Integer index = integerArray.length;
-        Integer[] retArr = Arrays.copyOf(integerArray,index+1);
+        Integer[] retArr = Arrays.copyOf(integerArray, index + 1);
 
-        retArr[retArr.length-1] = valueToBeAdded;
+        retArr[retArr.length - 1] = valueToBeAdded;
 
         return retArr;
     }
 
     /**
-     * @param integerArray - array to be manipulated
-     * @param indexToInsertAt - index of the element to be inserted at
+     * @param integerArray      - array to be manipulated
+     * @param indexToInsertAt   - index of the element to be inserted at
      * @param valueToBeInserted - value of the element to be inserted
      * @return `integerArray` with `valueToBeInserted` at index number `indexToInsertAt`
      */
     public static Integer[] replace(Integer[] integerArray, int indexToInsertAt, Integer valueToBeInserted) {
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i =0;i<integerArray.length;i++){
-            if(i!=indexToInsertAt){
+        for (int i = 0; i < integerArray.length; i++) {
+            if (i != indexToInsertAt) {
                 list.add(integerArray[i]);
-            }
-            else{
+            } else {
                 list.add(valueToBeInserted);
             }
         }
@@ -47,7 +46,7 @@ public class IntegerArrayUtils {
      * @return element located at `indexToFetch`
      */
     public static Integer get(Integer[] integerArray, Integer indexToFetch) {
-        return null;
+        return integerArray[indexToFetch];
     }
 
     /**
@@ -55,7 +54,18 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1 and odd-values decremented by 1
      */
     public static Integer[] incrementEvenDecrementOdd(Integer[] integerArray) {
-        return null;
+        Integer[] retArr = new Integer[integerArray.length];
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                Integer val = integerArray[i];
+                retArr[i] = val + 1;
+            } else {
+                Integer val = integerArray[i];
+                retArr[i] = val - 1;
+            }
+        }
+
+        return retArr;
     }
 
     /**
@@ -63,7 +73,18 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+        Integer[] retArr = new Integer[integerArray.length];
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                Integer val = integerArray[i];
+                retArr[i] = val + 1;
+            }
+            else{
+                retArr[i] = integerArray[i];
+            }
+        }
+        return retArr;
+
     }
 
     /**
@@ -71,6 +92,16 @@ public class IntegerArrayUtils {
      * @return identical array with odd-values decremented by 1
      */
     public static Integer[] decrementOdd(Integer[] input) {
-        return null;
+        Integer[] retArr = new Integer[input.length];
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+                Integer val = input[i];
+                retArr[i] = val - 1;
+            }
+            else{
+                retArr[i] = input[i];
+            }
+        }
+        return retArr;
     }
 }
--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,26 +1,31 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 28/11/2018.
  */
 public class StringArrayUtils {
     /**
      * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
-     * @param endingIndex - ending index of array
+     * @param startingIndex    - starting index of array to be spliced
+     * @param endingIndex      - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
     public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+        return Arrays.copyOfRange(arrayToBeSpliced, startingIndex, endingIndex);
+
     }
 
 
     /**
      * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
+     * @param startingIndex    - starting index of array to be spliced
      * @return an array all elements between after `startingIndex`
      */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+
+        return Arrays.copyOfRange(arrayToBeSpliced,startingIndex,arrayToBeSpliced.length);
     }
 }
